86b53276521558b3500ac059cd38e91f53bace86
ibasbase <muhammadnibras1@gmail.com>
2018-09-22 21:00:22 +0700

    Real Madrid Indonesia

---
+++ Hubungi_Kami.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/main_user.css"/>
+
+		<title>Distro Real Madrid</title>
+
+		<style type="text/css">
+			body{
+				margin-left: 0px;
+				margin-right: 0px;
+				margin-top: 0px;
+				margin-bottom: 0px;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div id="background">
+		<div id="container">
+			<div id="header">
+			<img src="header.jpg">
+			</div>
+			<div id="menu">
+				<ul>
+					<li><a href="Home.php">HOME</a></li>
+					<li><a href="#">JERSEY</a>
+						<ul>
+							<li><a href="Jersey.php">HOME & AWAY</a></li><br/>
+						</ul>
+					</li>
+					<li><a href="#">JAKET</a>
+						<ul>
+							<li><a href="Jacket">HOME & AWAY</a></li><br/>
+						</ul>
+					</li>
+					<li><a href="Hubungi_Kami.php">HUBUNGI KAMI</a></li>
+					<li><a href="#">TENTANG KAMI</a>
+						<ul>
+							<li><a href="Website.php">WEBSITE</a></li><br/>
+							<li><a href="Penulis.php">PENULIS</a></li><br/>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<div id="sidebar">
+			<img src="iklan.jpg">
+			</div>
+			<div id="content">
+			<img src="hubungikami.jpg">
+			</div>
+			<div id="footer">
+			<img src="footer1.jpg">
+			</div>
+		</div>		
+	</body>
+</html>
